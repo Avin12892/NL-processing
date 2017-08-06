38a2f2b0cda4da6d986a521d1ac81ca9fd04a31a
Jesse <jesse.spencer@me.com>
2017-08-06 19:39:30 -0400

    Classes brought in line with naming convention

--- nl_data.py
+++ nl_data.py
@@ -2,10 +2,11 @@ import nltk
 
 class TextAnalysis(object):
 
+
     def __init__(self, raw_text):
         self.raw_text = raw_text
 
-
+        
     def normalize(self, raw_text):
         tokens = nltk.word_tokenize(raw_text)
         return [word.lower() for word in tokens]
