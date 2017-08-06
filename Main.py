703d557453b45a2e3c78f056b80413c193e77e83
Jesse <jesse.spencer@me.com>
2017-08-06 16:19:42 -0400

    Initial
    Base files for project

---
+++ Main.py
@@ -0,0 +1,12 @@
+# !/Library/Frameworks/Python.framework/Versions/3.6/bin/pyvenv-3.6
+import sys
+# sys.path.insert(0, "/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages")
+from TextAnalysis import TextAnalysis
+
+if __name__ == '__main__':
+
+    # try:
+    #     file = open("/Users/Jesse/Code/Python workspace/Text Analysis/text.txt")
+    # except IOError:
+    #     print("Unable to open file.")
+    #     sys.exit()
---
+++ TextAnalysis.py
@@ -0,0 +1,25 @@
+import nltk
+
+class TextAnalysis(object):
+
+    def __init__(self, raw_text):
+        self.raw_text = raw_text
+
+
+    def normalize(self, raw_text):
+        tokens = nltk.word_tokenize(raw_text)
+        return [word.lower() for word in tokens]
+
+    def add_to_text(self, raw_text):
+        self.raw_text = self.raw_text + raw_text
+
+    def word_frequency(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    file = open("/Users/Jesse/Code/Python workspace/Text Analysis/text.txt")
+
+    textAnalysis = TextAnalysis(raw_text = file.read())
+    # print(textAnalysis.words)
