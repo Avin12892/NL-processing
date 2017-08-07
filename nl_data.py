4d5410916d9942799e553231f36f298db321b0f7
Jesse <jesse.spencer@me.com>
2017-08-06 21:38:56 -0400

    Split text processing into it's own class

--- nl_data.py
+++ nl_data.py
@@ -1,18 +1,10 @@
 import nltk
 
-class TextAnalysis(object):
-
+class NLData(object):
 
     def __init__(self, raw_text):
         self.raw_text = raw_text
 
-        
-    def normalize(self, raw_text):
-        tokens = nltk.word_tokenize(raw_text)
-        return [word.lower() for word in tokens]
-
-    def add_to_text(self, raw_text):
-        self.raw_text = self.raw_text + raw_text
 
     def word_frequency(self):
         pass
